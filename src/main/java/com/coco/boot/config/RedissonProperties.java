113567604f6efa40d638ee8c26e4374972606ef3
Hua <majicdh@gmail.com>
2024-03-18 16:03:26 +0800

    添加dockerfile文件  更改redisson初始化配置获取方式 更改upload接口返回结构

---
+++ src/main/java/com/coco/boot/config/RedissonProperties.java
@@ -0,0 +1,18 @@
+package com.coco.boot.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson配置
+ * @author Hua
+ * @since 2024-03-18 15:30
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.redisson")
+public class RedissonProperties {
+
+    private String file;
+}
