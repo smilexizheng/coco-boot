95b5ca0ea8d965937dd53b79f9cd470fceb938aa
Hua <majicdh@gmail.com>
2024-03-21 14:26:05 +0800

    跨域统一处理 返回请求头 Content-Type 统一处理为application/json; charset=utf-8

--- src/main/java/com/coco/boot/config/web/WebConfig.java
+++ src/main/java/com/coco/boot/config/web/WebConfig.java
@@ -2,6 +2,7 @@ package com.coco.boot.config.web;
 
 import com.coco.boot.interceptor.ChatInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ChatInterceptor())
                 .addPathPatterns("/v1/**"); // 这里可以自定义拦截的路径
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
